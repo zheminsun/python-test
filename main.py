3637f34fafdf6ccbeeb2f4162fca0bcf784aacc1
zheminsun <145761222+zheminsun@users.noreply.github.com>
2024-06-05 20:52:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
+import os
 import time
-import datetime
+import urllib.request
+import subprocess
+import json
+import tarfile
+import zipfile
+import psutil
+import signal
+from bottle import Bottle, run
 
-# 以下是一个简单的Python程序，使用一个无限循环来保持程序一直运行，并且每隔10分钟打印当前时间
+app = Bottle()
 
-while True:
-    # 在这里编写你的程序逻辑
-    current_time = datetime.datetime.now()
-    print("Current time:", current_time)
+@app.route('/ht')
+def read_root():
+    return {"message": "Hello, World!"}
 
-    # 暂停 10 分钟
-    time.sleep(600)  # 10分钟 = 600秒
+
+    
+
+if __name__ == "__main__":
+    run(app, host='0.0.0.0', port=10001)
